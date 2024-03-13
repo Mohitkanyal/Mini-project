394950244065da8e3d49232114e58b8d0bd677bf
Mohitkanyal <102403245+Mohitkanyal@users.noreply.github.com>
2024-03-14 02:00:18 +0530

    Add files via upload

---
+++ maharashtra.php
@@ -0,0 +1,142 @@
+<?php
+include("navbar.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/package.css?v=1">
+    
+</head>
+<body>
+<header >
+    <img src="./img/maha.jpg" alt="">
+    </header>
+
+       
+        <div class="effect">
+        <div class ="header-title">
+        <h1>Maharashtra </h1>
+        <!-- </div> -->
+        <!-- <div class="header-content"> -->
+<p>Overall, Maharashtra is a vibrant state known for its economic prowess, cultural richness, and diverse landscapes, making it a popular destination for travelers and a dynamic hub for commerce and industry in India.
+</p>        </div>
+
+    
+
+
+
+
+
+<!-- <section>
+    <div class="tour1">
+    <img class="card-img" src="./img/package1.jpg" />
+    <h1>
+        Vietnam
+    </h1>
+
+    </div>
+
+
+
+</section> -->
+
+    <div class="links">
+        <a href="#things">Things To do</a>
+        <a href="#time">Best time</a>
+        <a href="#know">FAQS</a>
+    </div>
+<!-- <h1 id="things">Things to do</h1> -->
+<!-- <h1 id="time">Best Time to visit</h1> -->
+
+
+<section class="events" id="things">
+            <div class="container">
+                <div class="title">
+                    <h1 >     Things to do</h1>
+                    <!-- <div class="line"></div> -->
+                </div>
+                <div class="box">
+                    <div class="card">
+                        <img class="card-img" src="./img/maha2.avif" />
+                        <h4>Mumbai</h4>
+                        <ul class="font-color">
+                           
+                        <li>3 Star Hotel </li>
+                        <li>Airport Pickup & Drop</li>
+                        <li>5 Activities</li>
+                        <li>Visa</li>
+                        <li>Selected Meals</li>
+                           
+                        </ul>
+                        <p>&#x20B9;20,586/per person</p>
+                        <a href="everest.php" class="bttn">Book Now</a>
+                    </div>
+                    <div class="card">
+                        <img class="card-img" src="./img/maha3.jpg" />
+                        <h4>Mahabaleshwar</h4>
+                        <ul class="font-color">
+                           
+                        <li>4 star,3 Star Hotels </li>
+                        <li>Airport Pickup & Drop</li>
+                        <li>3 Activities</li>
+                        <li>Visa</li>
+                        <li>Selected Meals</li>
+                           
+                        </ul>
+                        <p>&#x20B9;20,390/per person</p>
+                        <a href="everest.php" class="bttn">Book Now</a>
+                    </div>
+                    <div class="card">
+                        <img class="card-img" src="./img/maha4.avif" />
+                        <h4>Lonavala and Khandala</h4>
+                        <ul class="font-color">
+                           
+                        <li>4 Star Hotel </li>
+                        <li>Airport Pickup & Drop</li>
+                        <li>5 Activities</li>
+                        <li>Visa</li>
+                        <li>Selected Meals</li>
+                           
+                        </ul>
+                        <p>&#x20B9;24,148/per person</p>
+                        <a href="everest.php" class="bttn">Book Now</a>
+                        
+                    </div>
+                </div>
+            </div></section>
+
+<section id="time" >
+<div class="title">
+    <h1 >Best Time to Visit</h1>
+                
+</div>
+<div class="best-time">
+    <h1>November to February</h1>
+<p>The best time to visit Maharashtra is during the winter months, from November to February, when the weather is cooler and more pleasant for exploring the state's attractions.
+</p>
+</div>
+
+
+</section>
+
+<section id="know">
+<div class="title">
+    <h1 >Frequently Asked Questions</h1>
+                
+</div>
+    <div class="faqs">
+<h1>Q. What are the must-visit attractions in Maharashtra?</h1>
+<p>A: Some must-visit attractions include the Gateway of India in Mumbai, Ajanta and Ellora Caves near Aurangabad, Elephanta Caves near Mumbai, Lonavala and Khandala hill stations, and the hill station of Mahabaleshwar. </p>
+<h1>Q. Are there any trekking or adventure opportunities in Maharashtra?</h1>
+<p>A: Yes, Maharashtra offers several trekking and adventure opportunities, especially in the Western Ghats region. Popular trekking destinations include Harishchandragad, Rajmachi, and Lohagad forts, as well as the Bhimashankar Wildlife Sanctuary.</p>
+<h1>Q. What are some etiquette tips for travelers in Maharashtra?</h1>
+<p>A: It's important to respect local customs and traditions, dress modestly when visiting religious sites, and ask for permission before taking photographs of people. Learning a few basic Marathi phrases can also be appreciated by locals.</p></div>
+</section>
+            
+</body>
+</html>
