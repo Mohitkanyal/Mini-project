542bb950216d861ed9f8883ecab616ef280a8d3d
Mohitkanyal <102403245+Mohitkanyal@users.noreply.github.com>
2024-03-14 00:53:29 +0530

    Add files via upload

---
+++ singapore.php
@@ -0,0 +1,142 @@
+<?php
+include("navbar.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/package.css?v=1">
+    
+</head>
+<body>
+<header >
+    <img src="./img/singapore.avif" alt="">
+    </header>
+
+       
+        <div class="effect">
+        <div class ="header-title">
+        <h1>Singapore </h1>
+        <!-- </div> -->
+        <!-- <div class="header-content"> -->
+<p>Singapore is a popular tourist destination, attracting millions of visitors each year. Major attractions include Marina Bay Sands, Sentosa Island, Gardens by the Bay, Universal Studios Singapore, and the Singapore Zoo.</p>        
+</div>
+
+    
+
+
+
+
+
+<!-- <section>
+    <div class="tour1">
+    <img class="card-img" src="./img/package1.jpg" />
+    <h1>
+        Vietnam
+    </h1>
+
+    </div>
+
+
+
+</section> -->
+
+    <div class="links">
+        <a href="#things">Things To do</a>
+        <a href="#time">Best time</a>
+        <a href="#know">FAQS</a>
+    </div>
+<!-- <h1 id="things">Things to do</h1> -->
+<!-- <h1 id="time">Best Time to visit</h1> -->
+
+
+<section class="events" id="things">
+            <div class="container">
+                <div class="title">
+                    <h1 >     Things to do</h1>
+                    <!-- <div class="line"></div> -->
+                </div>
+                <div class="box">
+                    <div class="card">
+                        <img class="card-img" src="./img/singapore2.jpg" />
+                        <h4>Marina Barrage </h4>
+                        <ul class="font-color">
+                           
+                        <li>3 Star Hotel </li>
+                        <li>Airport Pickup & Drop</li>
+                        <li>5 Activities</li>
+                        <li>Visa</li>
+                        <li>Selected Meals</li>
+                           
+                        </ul>
+                        <p>&#x20B9;20,586/per person</p>
+                        <a href="everest.php" class="bttn">Book Now</a>
+                    </div>
+                    <div class="card">
+                        <img class="card-img" src="./img/singapore3.jpg" />
+                        <h4>Supertree Grove</h4>
+                        <ul class="font-color">
+                           
+                        <li>4 star,3 Star Hotels </li>
+                        <li>Airport Pickup & Drop</li>
+                        <li>3 Activities</li>
+                        <li>Visa</li>
+                        <li>Selected Meals</li>
+                           
+                        </ul>
+                        <p>&#x20B9;20,390/per person</p>
+                        <a href="everest.php" class="bttn">Book Now</a>
+                    </div>
+                    <div class="card">
+                        <img class="card-img" src="./img/singapore4.avif" />
+                        <h4>China Town</h4>
+                        <ul class="font-color">
+                           
+                        <li>4 Star Hotel </li>
+                        <li>Airport Pickup & Drop</li>
+                        <li>5 Activities</li>
+                        <li>Visa</li>
+                        <li>Selected Meals</li>
+                           
+                        </ul>
+                        <p>&#x20B9;24,148/per person</p>
+                        <a href="everest.php" class="bttn">Book Now</a>
+                        
+                    </div>
+                </div>
+            </div></section>
+
+<section id="time" >
+<div class="title">
+    <h1 >Best Time to Visit</h1>
+                
+</div>
+<div class="best-time">
+    <h1>March to early June and September to November</h1>
+<p>Europe enjoys a mild and temperate climate. Unique wind patterns and ocean currents keep Europe warmer than other landmasses at similar latitudes.</p>
+</div>
+
+
+</section>
+
+<section id="know">
+<div class="title">
+    <h1 >Frequently Asked Questions</h1>
+                
+</div>
+    <div class="faqs">
+<h1>Q. What is the best places to visit in Singapore?</h1>
+<p>A: The places are Marina Bay, Orchard Road, Chinatown, Little India, Sentosa Island ,etc. </p>
+<h1>Q. Is Singapore expensive to travel to from India?</h1>
+<p>A. Singapore can be moderately expensive for travelers from India, but budget options are available for accommodation, dining, and transportation.</p>
+<h1>Q. Does Singapore have good beaches?</h1>
+<p>A. Singapore has artificial beaches on Sentosa Island, offering recreational activities and a convenient beach experience.</p>
+</div>
+</section>
+            
+</body>
+</html>
---
+++ thailand.php
@@ -0,0 +1,143 @@
+<?php
+include("navbar.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/package.css?v=1">
+    
+</head>
+<body>
+<header >
+    <img src="./img/thailand.jpg" alt="">
+    </header>
+
+       
+        <div class="effect">
+        <div class ="header-title">
+        <h1>Thailand </h1>
+        <!-- </div> -->
+        <!-- <div class="header-content"> -->
+<p>Thailand, a Southeast Asian nation, is renowned for its stunning beaches, rich cultural heritage, vibrant cities, and delicious cuisine. With modern skyscrapers in Bangkok and serene islands like Phuket and Koh Samui, Thailand's warm hospitality and affordability make it a popular tourist destination.</p></div>
+    
+
+
+
+
+
+<!-- <section>
+    <div class="tour1">
+    <img class="card-img" src="./img/package1.jpg" />
+    <h1>
+        Vietnam
+    </h1>
+
+    </div>
+
+
+
+</section> -->
+
+    <div class="links">
+        <a href="#things">Things To do</a>
+        <a href="#time">Best time</a>
+        <a href="#know">FAQS</a>
+    </div>
+<!-- <h1 id="things">Things to do</h1> -->
+<!-- <h1 id="time">Best Time to visit</h1> -->
+
+
+<section class="events" id="things">
+            <div class="container">
+                <div class="title">
+                    <h1 >     Things to do</h1>
+                    <!-- <div class="line"></div> -->
+                </div>
+                <div class="box">
+                    <div class="card">
+                        <img class="card-img" src="./img/thailand2.webp" />
+                        <h4>Bangkok </h4>
+                        <ul class="font-color">
+                           
+                        <li>3 Star Hotel </li>
+                        <li>Airport Pickup & Drop</li>
+                        <li>5 Activities</li>
+                        <li>Visa</li>
+                        <li>Selected Meals</li>
+                           
+                        </ul>
+                        <p>&#x20B9;20,586/per person</p>
+                        <a href="everest.php" class="bttn">Book Now</a>
+                    </div>
+                    <div class="card">
+                        <img class="card-img" src="./img/thailand3.jpg" />
+                        <h4>Chiang Mai</h4>
+                        <ul class="font-color">
+                           
+                        <li>4 star,3 Star Hotels </li>
+                        <li>Airport Pickup & Drop</li>
+                        <li>3 Activities</li>
+                        <li>Visa</li>
+                        <li>Selected Meals</li>
+                           
+                        </ul>
+                        <p>&#x20B9;20,390/per person</p>
+                        <a href="everest.php" class="bttn">Book Now</a>
+                    </div>
+                    <div class="card">
+                        <img class="card-img" src="./img/thailand4.jpg" />
+                        <h4>Khao Sok National Park</h4>
+                        <ul class="font-color">
+                           
+                        <li>4 Star Hotel </li>
+                        <li>Airport Pickup & Drop</li>
+                        <li>5 Activities</li>
+                        <li>Visa</li>
+                        <li>Selected Meals</li>
+                           
+                        </ul>
+                        <p>&#x20B9;24,148/per person</p>
+                        <a href="everest.php" class="bttn">Book Now</a>
+                        
+                    </div>
+                </div>
+            </div></section>
+
+<section id="time" >
+<div class="title">
+    <h1 >Best Time to Visit</h1>
+                
+</div>
+<div class="best-time">
+    <h1>November to February</h1>
+<p>
+The best time to visit thailand is during cool season offers comfortable temperatures and minimal rainfall, ideal for exploring the country. Beach activities may be more enjoyable during the dry season.</div>
+
+
+</section>
+
+<section id="know">
+<div class="title">
+    <h1 >Frequently Asked Questions</h1>
+                
+</div>
+    <div class="faqs">
+<h1>Q. What is the best places to visit in Thailand?</h1>
+<p>A: The places are Bangkok,Chiang Mai,Phuket,Krabi,Ayutthaya,Sukhothai,Pai,Koh Samui,Koh Phi Phi,Pattaya.</p>
+<h1>Q. Is Thailand expensive to travel to from India?</h1>
+<p>A: Thailand can be affordable for travelers from India, offering various budget options for accommodation, dining, and activities.
+
+</p>
+<h1>Q. Does Thailand have good beaches?</h1>
+<p>A: 
+
+Yes, Thailand is renowned for its stunning beaches, offering pristine sands, clear turquoise waters, and a range of activities such as swimming, snorkeling, diving, and sunbathing. Popular beach destinations include Phuket, Krabi, Koh Samui, Koh Phi Phi, and many more.</p>
+</section>
+            
+</body>
+</html>
