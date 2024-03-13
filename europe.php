97790f266bb4c70588e2e922125a2b0c8c5c8193
Mohitkanyal <102403245+Mohitkanyal@users.noreply.github.com>
2024-03-13 23:00:16 +0530

    Create europe.php

---
+++ europe.php
@@ -0,0 +1,145 @@
+<?php
+include("navbar.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/package.css?v=1">
+    
+</head>
+<body>
+<header >
+    <img src="./img/europe.avif" alt="">
+    </header>
+
+       
+        <div class="effect">
+        <div class ="header-title">
+        <h1>Europe </h1>
+        <!-- </div> -->
+        <!-- <div class="header-content"> -->
+<p>Europe, primarily in the Northern Hemisphere, is bordered by the Arctic, Atlantic, and Mediterranean Seas, and is divided from Asia by the Ural and Caucasus Mountains, Ural River, and Caspian Seas.</p>
+</p>        </div>
+
+    
+
+
+
+
+
+<!-- <section>
+    <div class="tour1">
+    <img class="card-img" src="./img/package1.jpg" />
+    <h1>
+        Vietnam
+    </h1>
+
+    </div>
+
+
+
+</section> -->
+
+    <div class="links">
+        <a href="#things">Things To do</a>
+        <a href="#time">Best time</a>
+        <a href="#know">FAQS</a>
+    </div>
+<!-- <h1 id="things">Things to do</h1> -->
+<!-- <h1 id="time">Best Time to visit</h1> -->
+
+
+<section class="events" id="things">
+            <div class="container">
+                <div class="title">
+                    <h1 >     Things to do</h1>
+                    <!-- <div class="line"></div> -->
+                </div>
+                <div class="box">
+                    <div class="card">
+                        <img class="card-img" src="./img/europe2.avif" />
+                        <h4>Eiffel Tower</h4>
+                        <ul class="font-color">
+                           
+                        <li>3 Star Hotel </li>
+                        <li>Airport Pickup & Drop</li>
+                        <li>5 Activities</li>
+                        <li>Visa</li>
+                        <li>Selected Meals</li>
+                           
+                        </ul>
+                        <p>&#x20B9;20,586/per person</p>
+                        <a href="everest.php" class="bttn">Book Now</a>
+                    </div>
+                    <div class="card">
+                        <img class="card-img" src="./img/europe3.jfif" />
+                        <h4>Rome</h4>
+                        <ul class="font-color">
+                           
+                        <li>4 star,3 Star Hotels </li>
+                        <li>Airport Pickup & Drop</li>
+                        <li>3 Activities</li>
+                        <li>Visa</li>
+                        <li>Selected Meals</li>
+                           
+                        </ul>
+                        <p>&#x20B9;20,390/per person</p>
+                        <a href="everest.php" class="bttn">Book Now</a>
+                    </div>
+                    <div class="card">
+                        <img class="card-img" src="./img/europe4.jfif" />
+                        <h4>Swiss Alps</h4>
+                        <ul class="font-color">
+                           
+                        <li>4 Star Hotel </li>
+                        <li>Airport Pickup & Drop</li>
+                        <li>5 Activities</li>
+                        <li>Visa</li>
+                        <li>Selected Meals</li>
+                           
+                        </ul>
+                        <p>&#x20B9;24,148/per person</p>
+                        <a href="everest.php" class="bttn">Book Now</a>
+                        
+                    </div>
+                </div>
+            </div></section>
+
+<section id="time" >
+<div class="title">
+    <h1 >Best Time to Visit</h1>
+                
+</div>
+<div class="best-time">
+    <h1>March to early June and September to November</h1>
+<p>Europe enjoys a mild and temperate climate. Unique wind patterns and ocean currents keep Europe warmer than other landmasses at similar latitudes.</p>
+</div>
+
+
+</section>
+
+<section id="know">
+<div class="title">
+    <h1 >Frequently Asked Questions</h1>
+                
+</div>
+    <div class="faqs">
+<h1>Q. What is the best city to visit in Europe?</h1>
+<p>A: Some other cities to consider visiting include:
+Rome, Italy, Barcelona, Spain, Prague, Czech Republic, Dubrovnik, Croatia, Nice, France, Vienna. </p>
+<h1>Q. What is the rainy reason in Europe?</h1>
+<p>A.The rainy season in Europe is the winter months, but it varies by region. </p>
+<h1>Q. Is Europe expensive to travel to from India?</h1>
+<p>A.Malaysia is a reasonably affordable destination to visit. It is in the top 25% of countries in the world for its affordability. If you're traveling on a budget, then this is a good destination with affordable accommodation, food, and transportation.</p>
+<h1>Q. Does Europe have good beaches?</h1>
+<p>A. Yes, Europe has many beautiful beaches, including beaches with clear water, beaches with white sand, and beaches with unique features.</p>
+</div>
+</section>
+            
+</body>
+</html>
