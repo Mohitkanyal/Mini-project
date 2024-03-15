e69d342f1f997d43d15206a1771c74319ebad613
Mohitkanyal <102403245+Mohitkanyal@users.noreply.github.com>
2024-03-15 21:08:37 +0530

    Add files via upload

---
+++ dubai.php
@@ -0,0 +1,141 @@
+<?php
+include("navbar.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/package.css?v=1">
+    
+</head>
+<body>
+<header >
+    <img src="./img/dubai.avif" alt="">
+    </header>
+
+       
+        <div class="effect">
+        <div class ="header-title">
+        <h1>Dubai </h1>
+        <!-- </div> -->
+        <!-- <div class="header-content"> -->
+<p> Dubai, a United Arab Emirates city known for luxury shopping, modern architecture, and nightlife, features Burj Khalifa, Dubai Fountain, and Atlantis, The Palm resort on artificial islands.</p></div>
+
+    
+
+
+
+
+
+<!-- <section>
+    <div class="tour1">
+    <img class="card-img" src="./img/package1.jpg" />
+    <h1>
+        Vietnam
+    </h1>
+
+    </div>
+
+
+
+</section> -->
+
+    <div class="links">
+        <a href="#things">Things To do</a>
+        <a href="#time">Best time</a>
+        <a href="#know">FAQS</a>
+    </div>
+<!-- <h1 id="things">Things to do</h1> -->
+<!-- <h1 id="time">Best Time to visit</h1> -->
+
+
+<section class="events" id="things">
+            <div class="container">
+                <div class="title">
+                    <h1 >     Things to do</h1>
+                    <!-- <div class="line"></div> -->
+                </div>
+                <div class="box">
+                    <div class="card">
+                        <img class="card-img" src="./img/dubai2.avif" />
+                        <h4>Burj Khalifa </h4>
+                        <ul class="font-color">
+                           
+                        <li>3 Star Hotel </li>
+                        <li>Airport Pickup & Drop</li>
+                        <li>5 Activities</li>
+                        <li>Visa</li>
+                        <li>Selected Meals</li>
+                           
+                        </ul>
+                        <p>&#x20B9;20,586/per person</p>
+                        <a href="everest.php" class="bttn">Book Now</a>
+                    </div>
+                    <div class="card">
+                        <img class="card-img" src="./img/dubai3.jpg" />
+                        <h4>Atlantis, The Palm</h4>
+                        <ul class="font-color">
+                           
+                        <li>4 star,3 Star Hotels </li>
+                        <li>Airport Pickup & Drop</li>
+                        <li>3 Activities</li>
+                        <li>Visa</li>
+                        <li>Selected Meals</li>
+                           
+                        </ul>
+                        <p>&#x20B9;20,390/per person</p>
+                        <a href="everest.php" class="bttn">Book Now</a>
+                    </div>
+                    <div class="card">
+                        <img class="card-img" src="./img/dubai4.jpg" />
+                        <h4>Museum of The Future</h4>
+                        <ul class="font-color">
+                           
+                        <li>4 Star Hotel </li>
+                        <li>Airport Pickup & Drop</li>
+                        <li>5 Activities</li>
+                        <li>Visa</li>
+                        <li>Selected Meals</li>
+                           
+                        </ul>
+                        <p>&#x20B9;24,148/per person</p>
+                        <a href="everest.php" class="bttn">Book Now</a>
+                        
+                    </div>
+                </div>
+            </div></section>
+
+<section id="time" >
+<div class="title">
+    <h1 >Best Time to Visit</h1>
+                
+</div>
+<div class="best-time">
+    <h1>November to March</h1>
+<p>
+The best time to visit Dubai is during the cooler months from November to March, when the weather is pleasant for outdoor activities and sightseeing</p></div>
+
+
+</section>
+
+<section id="know">
+<div class="title">
+    <h1 >Frequently Asked Questions</h1>
+                
+</div>
+    <div class="faqs">
+<h1>Q. What is the best places to visit in Dubai?</h1>
+<p>A: The places are Burj Khalifa, Dubai Mall, Palm Jumeirah, Burj Al Arab, Dubai Marina, Dubai Creek, Dubai Fountain, Jumeirah Beach, Desert Safari, Dubai Frame. </p>
+<h1>Q. Is Dubai expensive to travel to from India?</h1>
+<p>A. Dubai can be moderately expensive for travelers from India, but budget options are available for accommodation, dining, and activities.</p>
+<h1>Q. Does Dubai have good beaches?</h1>
+<p>A: 
+Yes, Dubai has several beautiful beaches along its coastline, including Jumeirah Beach, Kite Beach, and Al Mamzar Beach Park. </p></div>
+</section>
+            
+</body>
+</html>
