b3e5dd0daf448b4726506da3e2daaabc69d440b3
Mohitkanyal <102403245+Mohitkanyal@users.noreply.github.com>
2024-03-25 16:06:57 +0530

    Add files via upload

---
+++ kualaLumpur.php
@@ -0,0 +1,166 @@
+<?php
+include("navbar.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/details.css?v=6">
+
+</head>
+<body>
+<header >
+    <img src="./img/malaysia3.webp" alt="">
+    </header>
+</div>
+    <div>
+        <div class="effect">
+        <div class ="header-title">
+        <h1>Kuala Lumpur</h1>
+        <!-- </div> -->
+        <!-- <div class="header-content"> -->
+            <p>Kuala Lumpur is a dynamic city that seamlessly blends tradition with modernity, offering visitors a unique and unforgettable experience.</p>
+        </div>
+        <a href="book.php" class="bttn">Book Now</a>
+
+        </div>
+
+
+
+
+
+
+
+
+
+    <div class="links">
+        <a href="#day1">Day 1</a>
+        <a href="#day2">Day 2</a>
+        <a href="#day3">Day 3</a>
+        <a href="#day4">Day 4</a>
+        <a href="#day5">Day 5</a>
+    </div>
+
+
+
+<section class="events" id="day1">
+            <div class="container">
+                <div class="title">
+                    <h1 > Day 1</h1>
+                    <!-- <div class="line"></div> -->
+                </div>
+                <div class="box">
+                    <div class="card">
+                        <!-- <img class="card-img" src="./img/packages/vietnam1.avif" /> -->
+                        <h4>Arrival and City Exploration</h4>
+                        <ul class="font-color">
+                           <li>Morning: Arrive at Kuala Lumpur International Airport (KLIA) in the morning and transfer to your hotel by taxi or airport.</li>
+                           <li>Afternoon: Check into your hotel and start your city exploration. Visit iconic landmarks such as the Petronas Twin Towers, located in the heart of the city.</li>
+                           <li>Evening: Explore the lively Bukit Bintang area, known for shopping malls, street food, and nightlife, and enjoy dinner at local eateries or various restaurants.</li>
+                        </ul>
+                        <p>Upon arrival at Kuala Lumpur International Airport, explore the city's vibrant atmosphere, Petronas Twin Towers, neighborhoods, local food, Bukit Bintang, and nightlife before retiring to your hotel.</p>
+                    </div>
+                   
+                </div>
+            </div></section>
+
+
+<section class="events" id="day2">
+            <div class="container">
+                <div class="title">
+                    <h1 >Day 2 </h1>
+                    <!-- <div class="line"></div> -->
+                </div>
+                <div class="box">
+                    <div class="card">
+                        <!-- <img class="card-img" src="./img/packages/vietnam1.avif" /> -->
+                        <h4>Cultural Exploration</h4>
+                        <ul class="font-color">
+                           
+                        <li>Morning: Visit the Batu Caves, a 13km north of Kuala Lumpur Hindu temple complex, where you can climb 272 steps to admire its stunning limestone formations.</li>
+                        <li>Afternoon: Explore the historic district of Kuala Lumpur, including Merdeka Square, Sultan Abdul Samad Building, and Jamek Mosque.</li>
+                        <li>Evening: Experience traditional Malay cuisine at a local restaurant or explore the vibrant food stalls at Jalan Alor, a famous street food area in Kuala Lumpur.</li>
+                        </ul>
+                        <p>Explore Kuala Lumpur's rich heritage, including Batu Caves, Merdeka Square, and Chinatown, while enjoying Malaysian cuisine and reflecting on discoveries.</p>
+                    </div>
+                   
+                </div>
+            </div></section>
+
+    <section class="events" id="day3">
+            <div class="container">
+                <div class="title">
+                    <h1 >Day 3 </h1>
+                    <!-- <div class="line"></div> -->
+                </div>
+                <div class="box">
+                    <div class="card">
+                        <!-- <img class="card-img" src="./img/packages/vietnam1.avif" /> -->
+                        <h4>Nature and Adventure</h4>
+                        <ul class="font-color">
+                           
+                        <li>Morning: Visit Kuala Lumpur Forest Eco Park for a morning hike in the tropical rainforest, taking in the city's panoramic skyline views from the canopy walkway.</li>
+                        <li>Afternoon: Visit the Kuala Lumpur Bird Park, located adjacent to the Kuala Lumpur Forest Eco Park. Explore the world's largest free-flight walk-in aviary and get up close with a variety of bird species.</li>
+                        <li>Evening: Relax and unwind with a leisurely dinner cruise along the Klang River, offering stunning views of Kuala Lumpur's illuminated skyline.</li>
+                        
+                           
+                        </ul>
+                        <p>Explore Kuala Lumpur's natural beauty and adventure by visiting the Kuala Lumpur Forest Eco Park, Kuala Lumpur Bird Park, Berjaya Times Square Theme Park, and a relaxing dinner at a nearby restaurant. Enjoy nature trails, bird shows, and a city skyline view.</p>
+                    </div>
+                   
+                </div>
+            </div></section>
+
+            <section class="events" id="day4">
+            <div class="container">
+                <div class="title">
+                    <h1 >Day 4 </h1>
+                    <!-- <div class="line"></div> -->
+                </div>
+                <div class="box">
+                    <div class="card">
+                        <!-- <img class="card-img" src="./img/packages/vietnam1.avif" /> -->
+                        <h4>Shopping and Entertainment</h4>
+                        <ul class="font-color">
+                           
+                        <li>Morning: Spend the morning exploring one of Kuala Lumpur's famous shopping districts, such as KLCC Suria, Pavilion Kuala Lumpur, or Mid Valley Megamall.</li>
+                        <li>Afternoon: Visit the Islamic Arts Museum Malaysia, located approximately 7 kilometers (about 4.3 miles) from the city center. Explore the museum's extensive collection of Islamic art and artifacts.</li>
+                        <li>Evening: Enjoy a cultural performance at the Istana Budaya (National Theatre) or catch a live music performance at one of Kuala Lumpur's entertainment venues.</li>
+                        </ul>
+                        <p>Take a leisurely stroll to end the day after exploring the Bukit Bintang retail district, Little India in Brickfields, Islamic Arts Museum Malaysia, and posh malls for cultural experiences and delectable Indian cuisine.</p>
+                    </div>
+                   
+                </div>
+            </div></section>
+
+            <section class="events" id="day5">
+            <div class="container">
+                <div class="title">
+                    <h1 >Day 5</h1>
+                    <!-- <div class="line"></div> -->
+                </div>
+                <div class="box">
+                    <div class="card">
+                        <!-- <img class="card-img" src="./img/packages/vietnam1.avif" /> -->
+                        <h4>Day Trip and Departure</h4>
+                        <ul class="font-color">
+                           
+                        <li>Morning: Visit Malacca, a UNESCO-listed city 150 kilometers south of Kuala Lumpur, to explore its renowned heritage sites like A Famosa fortress, Christ Church, and Jonker Street.</li>
+                        <li>Afternoon: Enjoy a traditional Peranakan lunch at one of Malacca's renowned restaurants.</li>
+                        <li>Evening: Return to Kuala Lumpur in the evening and spend your final night exploring the city or relaxing at your hotel.</li>
+                        </ul>
+                        <p>On your final day in Kuala Lumpur, explore Malacca's rich history and cultural heritage, visiting UNESCO-listed sites, enjoying traditional Peranakan lunch, visiting museums, and taking a cruise along the Malacca River before returning to Kuala Lumpur.</p>
+                    </div>
+                   
+                </div>
+            </div></section>
+
+
+
+</section>
+</body>
+</html>
---
+++ langkawi.php
@@ -0,0 +1,144 @@
+<?php
+include("navbar.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/details.css?v=7">
+
+</head>
+<body>
+<header >
+    <img src="./img/langkawi.jpg" alt="">
+    </header>
+</div>
+    <div>
+        <div class="effect">
+        <div class ="header-title">
+        <h1>Langkawi</h1>
+        <!-- </div> -->
+        <!-- <div class="header-content"> -->
+            <p>Langkawi offers a perfect blend of natural beauty, cultural heritage, and modern amenities, making it a must-visit destination for travelers seeking an idyllic tropical getaway.</p>
+        </div>
+        <a href="book.php" class="bttn">Book Now</a>
+
+        </div>
+
+
+
+
+
+
+
+
+
+    <div class="links">
+        <a href="#day1">Day 1</a>
+        <a href="#day2">Day 2</a>
+        <a href="#day3">Day 3</a>
+        <a href="#day4">Day 4</a>
+    </div>
+
+
+
+<section class="events" id="day1">
+            <div class="container">
+                <div class="title">
+                    <h1 > Day 1</h1>
+                    <!-- <div class="line"></div> -->
+                </div>
+                <div class="box">
+                    <div class="card">
+                        <!-- <img class="card-img" src="./img/packages/vietnam1.avif" /> -->
+                        <h4>Arrival and Beach Relaxation</h4>
+                        <ul class="font-color">
+                           <li>Morning: Arrive at Langkawi International Airport, which is located near the main tourist areas. Take a taxi or rent a car to your hotel.</li>
+                           <li>Afternoon: After your journey, relax at your hotel and explore Pantai Cenang, a popular beach in Langkawi, for sun, swimming, and water sports.</li>
+                           <li>Evening: Enjoy dinner at one of the beachfront restaurants along Pantai Cenang, savoring fresh seafood and local Malay dishes while watching the sunset over the Andaman Sea.</li>
+                        </ul>
+                        <p>Arrive at Langkawi Airport, transfer to hotel near Pantai Cenang. Enjoy beach relaxation, dips in Andaman Sea, seafood dinner at beachfront restaurant, and restful evening for future adventures.</p>
+                    </div>
+                   
+                </div>
+            </div></section>
+
+
+<section class="events" id="day2">
+            <div class="container">
+                <div class="title">
+                    <h1 >Day 2 </h1>
+                    <!-- <div class="line"></div> -->
+                </div>
+                <div class="box">
+                    <div class="card">
+                        <!-- <img class="card-img" src="./img/packages/vietnam1.avif" /> -->
+                        <h4>Cultural Exploration</h4>
+                        <ul class="font-color">
+                           
+                        <li>Morning: Start your day with an island hopping tour, stopping at Pulau Dayang Bunting, Pulau Singa Besar, and Pulau Beras Basah for swimming, snorkeling, and beach exploration.</li>
+                        <li>Afternoon:Explore Kilim Karst Geoforest Park in Langkawi, showcasing mangrove forests, limestone caves, and narrow waterways, and observing wildlife like eagles and monkeys.</li>
+                        <li>Evening: Return to your hotel and relax after a day of adventure. Treat yourself to dinner at a local restaurant, perhaps trying some traditional Malay cuisine like nasi lemak or laksa.</li>
+                        </ul>
+                        <p>Experience an island hopping tour of Pulau Dayang Bunting, Pulau Singa Besar, and Pulau Beras Basah, followed by a mangrove boat tour through Kilim Karst Geoforest Park.</p>
+                    </div>
+                   
+                </div>
+            </div></section>
+
+    <section class="events" id="day3">
+            <div class="container">
+                <div class="title">
+                    <h1 >Day 3 </h1>
+                    <!-- <div class="line"></div> -->
+                </div>
+                <div class="box">
+                    <div class="card">
+                        <!-- <img class="card-img" src="./img/packages/vietnam1.avif" /> -->
+                        <h4>Cable Car Ride and Sky Bridge</h4>
+                        <ul class="font-color">
+                           
+                        <li>Morning: Visit Oriental Village for a thrilling cable car ride to Gunung Mat Cincang, Langkawi's second-highest peak, offering panoramic views of surrounding islands and dense rainforest.</li>
+                        <li>Afternoon: Visit the top station's attractions, including the Sky Bridge, for breathtaking views and take photos before descending back down.</li>
+                        <li>Evening: Return to Pantai Cenang and spend the evening relaxing on the beach or exploring the nearby shops and restaurants. Consider booking a sunset dinner cruise for a memorable end to the day.</li>
+                        
+                           
+                        </ul>
+                        <p>Visit Oriental Village for cable car ride to Gunung Mat Cincang peak, explore Sky Bridge, return to Pantai Cenang for beach relaxation, stroll, shop, dine, enjoy sunset views, and enjoy dinner.</p>
+                    </div>
+                   
+                </div>
+            </div></section>
+
+            <section class="events" id="day4">
+            <div class="container">
+                <div class="title">
+                    <h1 >Day 4 </h1>
+                    <!-- <div class="line"></div> -->
+                </div>
+                <div class="box">
+                    <div class="card">
+                        <!-- <img class="card-img" src="./img/packages/vietnam1.avif" /> -->
+                        <h4>Waterfall Hike and Cultural Exploration</h4>
+                        <ul class="font-color">
+                           
+                        <li>Morning: Visit Telaga Tujuh Waterfalls, also known as Seven Wells Waterfalls, 22 kilometers from Pantai Cenang, where hike through rainforest to reach natural pools and admire cascading water.</li>
+                        <li>Afternoon: Discover Langkawi's cultural treasures like the Langkawi Craft Complex and Mahsuri's Tomb, where you can learn about traditional crafts and immerse yourself in the island's rich heritage.</li>
+                        <li>Evening: Enjoy a farewell dinner at a restaurant of your choice, reflecting on your time in Langkawi and savoring your last evening on the island.</li>
+                        </ul>
+                        <p>Explore Telaga Tujuh Waterfalls, enjoy natural pools, and explore Langkawi's cultural attractions. Enjoy a farewell dinner at a local restaurant, reminiscing about Langkawi experiences and Malaysian cuisine.</p>
+                    </div>
+                   
+                </div>
+            </div></section>
+
+
+
+
+</section>
+</body>
+</html>
